359d1ee12ae63b3a2062bc3c01495f6b7e08a142
Quinn <neochatics@protonmail.com>
2022-02-22 18:43:52 -0500

    Vision

--- Team3123/src/main/java/org/firstinspires/ftc/team3123/c_vision/pipeline.java
+++ Team3123/src/main/java/org/firstinspires/ftc/team3123/c_vision/pipeline.java
@@ -11,20 +11,18 @@ public class pipeline extends OpenCvPipeline {
 	public Scalar lower = new Scalar(0, 130, 0);
 	public Scalar upper = new Scalar(255, 255, 100);
 
-	private Mat matYCrCb       = new Mat();
-	private Mat binaryMat      = new Mat();
-	private Mat maskedInputMat = new Mat();
-
-
+	private final Mat matYCrCb       = new Mat();
+	private final Mat matBinary      = new Mat();
+	private final Mat matMaskedInput = new Mat();
 
 	@Override
 	public Mat processFrame(Mat input) {
 		Imgproc.cvtColor(input, matYCrCb, Imgproc.COLOR_RGB2YCrCb);
-		Core.inRange(matYCrCb, lower, upper, binaryMat);
-		maskedInputMat.release();
-		Core.bitwise_and(input, input, maskedInputMat, binaryMat);
+		Core.inRange(matYCrCb, lower, upper, matBinary);
+		matMaskedInput.release();
+		Core.bitwise_and(input, input, matMaskedInput, matBinary);
 
-		return maskedInputMat;
+		return matMaskedInput;
 	}
 
 }
